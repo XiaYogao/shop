ae2aa046b3d0a4c50c4372acab4fb421c04675ec
SharyGus <SharyGus@gmail.com>
2016-05-25 00:26:01 +0800

    commit code

---
+++ book-shop/src/main/java/com/scu/book/shop/entity/User.java
@@ -0,0 +1,19 @@
+package com.scu.book.shop.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1520713631595860663L;
+
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
